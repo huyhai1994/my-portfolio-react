dbf37fa4e50d997a717a0668d03b531419d84ffb
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-07-29 13:49:59 +0700

    using react hook to change the close and open menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,10 +3,18 @@ import styles from './Navbar.module.css'
 import {getImageUrl} from "../../utils.js";
 
 const Navbar = () => {
+    const [menuOpen, setMenuOpen] = React.useState(false);
+
     return (<nav className={styles.navbar}>
         <a className={styles.title} href="/">Portfolio</a>
         <div className={styles.menu}>
-            <img className={styles.menuBtn} src={getImageUrl('nav/menuIcon.png')} alt="Logo"/>
+            <img
+                className={styles.menuBtn}
+                src={menuOpen ?
+                    getImageUrl('nav/closeIcon.png') :
+                    getImageUrl('nav/menuIcon.png')}
+                onClick={() => setMenuOpen(!menuOpen)}
+                alt="Logo"/>
             <ul className={styles.menuItems}>
                 <li><a href="#about">About</a></li>
                 <li><a href="#experience">Experience</a></li>
