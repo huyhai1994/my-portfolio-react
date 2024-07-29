48cf7aa7701600ce2d440d3e88c704420353d5a8
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-07-29 13:11:57 +0700

    basic navbar structure (no css)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import styles from './App.module.css'
 import '@fontsource/outfit'
 import '@fontsource/roboto'
+import Navbar from "./components/Navbar/Navbar.jsx";
 
 function App() {
 
     return (<div className={styles.App}>
-        Hello World!
+        <Navbar/>
     </div>)
 }
 
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (<nav>
+        <a href="/">Portfolio</a>
+        <div>
+            <ul>
+                <li><a href="#about">About</a></li>
+            </ul>
+            <ul>
+                <li><a href="#experience">Experience</a></li>
+            </ul>
+            <ul>
+                <li><a href="#project">Projects</a></li>
+            </ul>
+            <ul>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+
+        </div>
+    </nav>)
+}
+export default Navbar
