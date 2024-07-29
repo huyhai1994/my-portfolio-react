367eafac7b6028a3f5268981e75e1c921465c951
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-07-29 12:29:23 +0700

    init project

---
+++ README.md
@@ -0,0 +1,7 @@
+## Online Portfolio
+
+### Setup
+
+    1. npm install;
+    2. Install fonts: npm install @fontsource/roboto @fontsource/outfit;
+    3. 
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.module.css'
+import '@fontsource/outfit'
+import '@fontsource/roboto'
+
+function App() {
+
+    return (<>
+        Hello World!
+    </>)
+}
+
+export default App
