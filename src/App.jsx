00322931b6ce5d12fe70238212a565a5937afb7b
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-07-31 16:20:27 +0700

    css About.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import '@fontsource/outfit'
 import '@fontsource/roboto'
 import Navbar from "./components/Navbar/Navbar.jsx";
 import Hero from "./components/Hero/Hero.jsx";
+import About from "./components/About/About.jsx";
 
 function App() {
 
     return (<div className={styles.App}>
         <Navbar/>
         <Hero/>
+        <About/>
     </div>)
 }
 
---
+++ src/components/About/About.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import {getImageUrl} from "../../utils.js";
+import styles from './About.module.css'
+
+const About = () => {
+    return (<section className={styles.container}>
+        <h2 className={styles.title}>
+            About Me
+        </h2>
+        <div className={styles.content}>
+            <img src={getImageUrl("about/aboutImage.png")} alt="Me sitting with the laptop"
+                 className={styles.aboutImage}/>
+            <ul className={styles.aboutItems}>
+                <li className={styles.aboutItem}>
+                    <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon"/>
+                    <div className={styles.aboutItemText}>
+                        <h3>Front end developer</h3>
+                        <p>I'm a front end developer</p>
+                    </div>
+                </li>
+                <li className={styles.aboutItem}>
+                    <img src={getImageUrl("about/serverIcon.png")} alt="Server icon"/>
+                    <div className={styles.aboutItemText}>
+                        <h3>Back end developer</h3>
+                        <p>I'm a back end developer</p>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </section>)
+}
+export default About
